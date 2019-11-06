694821c347515283221ec7100ba7e91edae501e1
Jannis Pohlmann <jannis@thegraph.com>
2019-11-07 00:39:31 +0100

    Pull issues into flat view

--- components/views/flat-view.tsx
+++ components/views/flat-view.tsx
@@ -1,7 +1,25 @@
 import { FlatView } from '../../utils/config'
+import { useConfig } from '../../hooks/config'
+import { useOrganization } from '../../hooks/organization'
+import { useIssues } from '../../hooks/issues'
 
 const View: React.FunctionComponent<{ view: FlatView }> = ({ view }) => {
-  return <div>Flat view</div>
+  const { data: config } = useConfig()
+
+  let issues = config.repositories.reduce((issues, repository) => {
+    let { data: repoIssues } = useIssues(config.organization, repository)
+    return issues.concat(repoIssues)
+  }, [])
+
+  return (
+    <div>
+      {issues.map(issue => (
+        <div>
+          {issue.number}: {issue.title}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default View
