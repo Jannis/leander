1aef1352953f37b225d0e537eaf5cd78ea9fc8aa
Jannis Pohlmann <jannis@thegraph.com>
2019-11-09 23:12:36 +0100

    Add queryGitHub function instead of useGitHub hook

--- hooks/github.ts
+++ hooks/github.ts
@@ -1,36 +1,50 @@
 import { useQuery } from 'react-query'
 import { GraphQLClient } from 'graphql-request'
+import { useRouter } from 'next/router'
 
 export const useGitHubAccessToken = () => {
-  let result = useQuery('github-access-token', async () => {
-    let result = await fetch('/auth/github/access-token')
-    try {
-      let data = await result.json()
-      return data.accessToken
-    } catch (e) {
-      return undefined
-    }
-  })
+  let { query } = useRouter()
+
+  let result = useQuery(
+    'github-access-token',
+    async () => {
+      let result = await fetch('/auth/github/access-token')
+      try {
+        let data = await result.json()
+        return data.accessToken
+      } catch (e) {
+        return undefined
+      }
+    },
+    {
+      refetchInterval: false,
+    },
+  )
 
   if (!result.data) {
-    window.location.href = '/auth/github'
+    if (query.config) {
+      window.location.href = `/auth/github/?config=${query.config}`
+    } else {
+      window.location.href = '/auth/github'
+    }
   }
 
   return result
 }
 
-export const useGitHubQuery = (query: string, variables: { [key: string]: any }) => {
-  const { data: accessToken } = useGitHubAccessToken()
-
-  return useQuery(
-    ['github-graphql', { query, variables }],
-    async ({ query, variables }) => {
-      const client = new GraphQLClient('https://api.github.com/graphql', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      return client.request(query, variables)
+export const queryGitHub = ({
+  query,
+  variables,
+  accessToken,
+}: {
+  query: string
+  variables: { [key: string]: any }
+  accessToken: string
+}) => {
+  const client = new GraphQLClient('https://api.github.com/graphql', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
     },
-  )
+  })
+  return client.request(query, variables)
 }
