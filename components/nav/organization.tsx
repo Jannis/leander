85d2949d95d89afda00d567e6ca9288180b06373
Jannis Pohlmann <jannis@thegraph.com>
2019-11-14 02:41:23 +0100

    Use new non-suspense useOrganization hook in navigation

--- components/nav/organization.tsx
+++ components/nav/organization.tsx
@@ -2,10 +2,26 @@ import { useConfig } from '../../hooks/config'
 import { useOrganization } from '../../hooks/organization'
 import GitHubLink from './github-link'
 import NavSection from './nav-section'
+import Spinner from '../spinner'
+import { InlineError } from '../errors'
 
 const Organization: React.FunctionComponent<{}> = props => {
   const config = useConfig()
-  const organization = useOrganization(config.organization)
+
+  const { loading, error, data } = useOrganization(
+    { login: config.organization },
+    { skip: false },
+  )
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  if (error) {
+    return <InlineError error={error} />
+  }
+
+  let organization = data && data.organization
 
   return (
     <NavSection title="Organization">
